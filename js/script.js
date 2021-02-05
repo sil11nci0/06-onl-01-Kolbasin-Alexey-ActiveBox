03f59c103301acaef3426c8e378f41505f702df8
sil11nci0 <alexey.alexandrovich@protonmail.ch>
2021-02-06 00:54:13 +0300

    Need fix section team

--- js/script.js
+++ js/script.js
@@ -11,3 +11,26 @@ $(document).ready(function () {
     appendDots: $('.clients__dots'),
   })
 })
+
+/* $(document).ready(function () {
+  $('.team__slider').slick({
+    arrows: true,
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 1000,
+    easing: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000, 
+    responsive: [
+      {
+        breakpoint: 1082,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          appendArrows: $('.team__arrows'),
+        },
+      },
+    ],
+  })
+}) */
